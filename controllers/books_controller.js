de4fd03c90ebb70a5121b3f80d6b3f23ce13278b
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-20 23:35:44 +0200

    configured books controllers and routes

---
+++ controllers/books_controller.js
@@ -0,0 +1,60 @@
+const database = require("../database");
+
+exports.getAllBooks = (req, res) => {
+    database.execute(
+        "SELECT * FROM books",
+        (err, result) => {
+            if (err) {
+                res.status(500).send({ error: "An error has occurred while fetching books" });
+            } else {
+                res.status(200).send(result);
+            }
+        }
+    );
+};
+
+exports.getBookById = (req, res) => {
+    const bookId = req.params.id;
+    database.execute(
+        "SELECT * FROM books WHERE book_id = ?",
+        [ bookId ],
+        (err, result) => {
+            if (err) {
+                res.status(500).send({ error: `An error has occurred while fetching book with id ${bookId}` });
+            } else {
+                res.status(200).send(result);
+            }
+        }
+    )
+}
+
+exports.addNewBook = (req, res) => {
+    const { title, author, description, publicationYear, addedBy } = req.body;
+    database.execute(
+        "INSERT INTO books (title, author, description, publication_year, added_by) VALUES (?, ?, ?, ?, ?)",
+        [ title, author, description, publicationYear, addedBy ],
+        (err, result) => {
+            if (err) {
+                res.status(500).send({ error: "An error has occurred while adding book" });
+            } else {
+                res.status(200).send(result);
+            }
+        }
+    );
+};
+
+exports.updateBook = (req, res) => {
+    const bookId = req.params.id;
+    const { title, author, description, publicationYear } = req.body;
+    database.execute(
+        "UPDATE books SET title = ?, author = ?, description = ?, publication_year = ? WHERE book_id = ?",
+        [ title, author, description, publicationYear, bookId ],
+        (err, result) => {
+            if (err) {
+                res.status(500).send({ error: `An error has occurred while updating book with id: ${bookId}` })
+            } else {
+                res.status(200).send(result);
+            }
+        }
+    );
+};
---
+++ routes/books_routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const booksController = require("../controllers/books_controller");
+
+const router = express.Router();
+
+router.get("/", booksController.getAllBooks);
+router.get("/:id", booksController.getBookById);
+router.post("/", booksController.addNewBook);
+router.put("/:id", booksController.updateBook);
+
+module.exports = router
