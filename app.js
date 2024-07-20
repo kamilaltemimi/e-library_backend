ff9631a869db1368c3851b3fbeca2b9c38412c83
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-20 16:14:55 +0200

    configured users routes and app.js with users routes

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
+const usersRoutes = require("./routes/users_routes")
+
+app.use("/users", usersRoutes)
+
 app.listen(3000);
