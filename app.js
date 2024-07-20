e164fcfcfe31774f1d27cdeb2567981a1ea266a7
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-20 23:35:23 +0200

    cinfigured routes in app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const usersRoutes = require("./routes/users_routes")
+const usersRoutes = require("./routes/users_routes");
+const booksRoutes = require("./routes/books_routes");
+const ratingRoutes = require("./routes/rating_routes");
 
-app.use("/users", usersRoutes)
+app.use("/users", usersRoutes);
+app.use("/books", booksRoutes);
+app.use("/rating", ratingRoutes);
 
 app.listen(3000);
