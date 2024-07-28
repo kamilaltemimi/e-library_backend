bcdf4c0f3bf10abc44dd17998836c9d72f8fbf06
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-28 19:57:34 +0200

    configured routes

--- routes/books_routes.js
+++ routes/books_routes.js
@@ -6,10 +6,11 @@ const router = express.Router();
 
 router.get("/", booksController.getAllBooks);
 router.get("/:id", booksController.getBookById);
-router.get("/borrowed-books/:id/:bookId", booksController.getBorrowedBooks);
+router.get("/borrowed-books/:id", booksController.getBorrowedBooksByUser);
 router.post("/", booksController.addNewBook);
-router.post("/borrowed-books/:id", booksController.borrowBook);
+router.post("/borrowed-books/:userId", booksController.borrowBook);
 router.put("/:id", booksController.updateBook);
 router.delete("/:id", booksController.deleteBook);
+router.delete("/borrowed-books/:userId/:bookId", booksController.returnBorrowedBook);
 
 module.exports = router
