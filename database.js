e35c9c63533fba2877ca4e43817faf5532d022ac
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-31 18:46:37 +0100

    added dotenv to hide database details

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const database = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "e_library",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = database;
